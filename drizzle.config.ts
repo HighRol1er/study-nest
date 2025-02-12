5249067e5bdd7bb0f2a9062cd6156bc9e53cae9b
HighRol1er <hunterjoe9999@gmail.com>
2025-02-12 15:58:06 +0900

    세팅은 끝

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,10 +5,10 @@ export default {
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'hunterjoe98',
-    database: 'culture_db',
+    host: process.env.DB_HOST || 'localhost', // 환경변수에서 호스트 가져오기 
+    port: Number(process.env.DB_PORT) || 5432, // 환경변수에서 포트 가져오기
+    user: process.env.DB_USER || 'postgres', // 환경변수에서 사용자 가져오기
+    password: process.env.DB_PASSWORD || 'postgres', // 환경변수에서 비밀번호 가져오기
+    database: process.env.DB_NAME || 'culture_db', // 환경변수에서 데이터베이스 이름 가져오기
   },
 } satisfies Config;   
